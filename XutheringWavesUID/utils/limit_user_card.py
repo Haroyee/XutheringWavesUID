07c97b1400be90ffd4c335eef4adbc00da86df60
Loping151 <wangkailing151@gmail.com>
2025-12-07 21:24:23 +0800

    极限面板不再每次加载

--- XutheringWavesUID/utils/limit_user_card.py
+++ XutheringWavesUID/utils/limit_user_card.py
@@ -3,22 +3,14 @@ from pathlib import Path
 
 import aiofiles
 
-from ..utils.resource.RESOURCE_PATH import PLAYER_PATH, MAP_PATH
+from ..utils.resource.RESOURCE_PATH import MAP_PATH
 
 LIMIT_PATH = MAP_PATH / "1.json"
 
 
 async def load_limit_user_card():
+    """每次读取极限面板数据，不再预加载"""
     async with aiofiles.open(LIMIT_PATH, "r", encoding="UTF-8") as f:
         data = json.loads(await f.read())
-
-    LIMIT_USER_PATH = PLAYER_PATH / "1"
-    if not LIMIT_USER_PATH.exists():
-        LIMIT_USER_PATH.mkdir(parents=True, exist_ok=True)
-
-    async with aiofiles.open(
-        LIMIT_USER_PATH / "rawData.json", "w", encoding="UTF-8"
-    ) as f:
-        await f.write(json.dumps(data, ensure_ascii=False))
 
     return data
